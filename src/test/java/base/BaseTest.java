ead7b4d0640434ad814259b3111aeed985be250e
Vladimir_Ask <vovanask2@gmail.com>
2024-11-08 09:21:23 +0300

    code refactoring

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -7,6 +7,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import pages.*;
 
+import static util.GetBoolean.getBooleanFromString;
+import static util.PropertiesUtil.getProp;
+
 public class BaseTest {
     protected WebDriver driver = CommonActions.createDriver();
     protected BasePage basePage = new BasePage(driver);
@@ -17,13 +20,19 @@ public class BaseTest {
 
     @AfterTest
     public void clearCookiesAndLocalStorage() {
-        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-        driver.manage().deleteAllCookies();
-        jsExecutor.executeScript("window.sessionStorage.clear();");
+        boolean clearCookiesAndStorage = getBooleanFromString(getProp("clear_cookies_and_storage"));
+        if (clearCookiesAndStorage) {
+            JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+            driver.manage().deleteAllCookies();
+            jsExecutor.executeScript("window.sessionStorage.clear();");
+        }
     }
 
     @AfterSuite(alwaysRun = true)
     public void close() {
-        driver.quit();
+        boolean closeBrowser = getBooleanFromString(getProp("close_browser"));
+        if (closeBrowser) {
+            driver.quit();
+        }
     }
 }
---
+++ src/test/java/util/GetBoolean.java
@@ -0,0 +1,12 @@
+package util;
+
+public class GetBoolean {
+
+    public static boolean getBooleanFromString(String str) {
+        if (str.equals("true")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
